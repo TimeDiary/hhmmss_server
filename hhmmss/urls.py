225dd2008609d6469b708ffcf0becb6b7d130658
Jho Lee <jooho_lee@outlook.kr>
2018-11-05 23:00:10 +0900

    Add test page

--- hhmmss/urls.py
+++ hhmmss/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('testPages.urls')),
+    path('', include('testPage.urls')),
     path('admin/', admin.site.urls),
 ]
