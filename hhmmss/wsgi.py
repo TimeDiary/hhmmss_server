664aafea4f2ab84bde49e1bd2b4bce43058bb38e
Jho Lee <jooho_lee@outlook.kr>
2018-11-05 22:56:10 +0900

    Add custom user model

--- hhmmss/wsgi.py
+++ hhmmss/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TimeDiary.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hhmmss.settings')
 
 application = get_wsgi_application()
